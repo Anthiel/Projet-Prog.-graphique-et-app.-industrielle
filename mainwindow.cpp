065c33a30520426010f9ec153fcce4348d491c76
Anthiel <44339217+Anthiel@users.noreply.github.com>
2019-09-25 14:53:14 +0200

    TP3 S2 MGM

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,305 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <math.h>
+
+/* **** début de la partie à compléter **** */
+
+float MainWindow::faceArea(MyMesh* _mesh, int faceID)
+{
+    FaceHandle fh = _mesh->face_handle(faceID);
+    std::vector <int> pointID;
+    VectorT <float,3> points[3];
+
+    for (MyMesh::FaceVertexIter curVert = _mesh->fv_iter(fh); curVert.is_valid(); curVert ++)
+    {
+        pointID.push_back((*curVert).idx());
+    }
+
+    for (int i=0; i<pointID.size();i++)
+    {
+        VertexHandle vh = _mesh->vertex_handle(pointID.at(i));
+        points[i][0] = _mesh->point(vh)[0];
+        points[i][1] = _mesh->point(vh)[1];
+        points[i][2] = _mesh->point(vh)[2];
+    }
+    VectorT<float,3> BmoinsA = points[1] - points[0];
+    VectorT<float,3> CmoinsA = points[2] - points[0];
+
+    VectorT<float,3> res;
+    res[0] = BmoinsA[1] * CmoinsA[2] - BmoinsA[2] * CmoinsA[1];
+    res[1] = BmoinsA[2] * CmoinsA[0] - BmoinsA[0] * CmoinsA[2];
+    res[2] = BmoinsA[0] * CmoinsA[1] - BmoinsA[1] * CmoinsA[0];
+
+    return res.norm()/2.0;
+}
+
+float MainWindow::angleFF(MyMesh* _mesh, int faceID0,  int faceID1, int vertID0, int vertID1)
+{
+     if(faceID0 == faceID1)
+         return 0.0;
+     if(vertID0 == vertID1)
+         return 0.0;
+
+     VertexHandle vh0 = _mesh->vertex_handle (vertID0);
+     FaceHandle fh0 = _mesh->face_handle (faceID0);
+     FaceHandle fh1 = _mesh->face_handle (faceID1);
+     int Signe = 0;
+
+     MyMesh::FaceVertexCWIter fh = _mesh->fv_cwiter (fh0);
+
+     while ( fh.is_valid() && *fh != vh0 )
+         fh++;
+     fh++;
+     VertexHandle Suivant = *fh;
+
+     if (Suivant.idx () == vertID1)
+         Signe = -1;
+     else
+         Signe = 1;
+
+     OpenMesh::Vec3f normal0 (_mesh->normal(fh0));
+     OpenMesh::Vec3f normal1 (_mesh->normal(fh1));
+
+     float scalar = normal0 | normal1;
+     return Signe * acos ( scalar );
+}
+
+VectorT <float,6> MainWindow::VecteurDirecteursTriangle(MyMesh *_mesh, int vertexID, int faceID){
+    FaceHandle fh = _mesh->face_handle(faceID);
+    std::vector <int> pointID;
+    VectorT <float,3> points[3];
+    int A = -1;
+    int C = -1, B = -1;
+
+     // On donne au point A son ID
+    for (MyMesh::FaceVertexIter curVert = _mesh->fv_iter(fh); curVert.is_valid(); curVert ++)
+    {
+       pointID.push_back((*curVert).idx());
+       if(vertexID == (*curVert).idx())
+           A = pointID.size()-1;
+    }
+
+    // affectation des points trouvés
+    for (int i=0; i<pointID.size();i++)
+    {
+        VertexHandle vh = _mesh->vertex_handle(pointID.at(i));
+        points[i][0] = _mesh->point(vh)[0];
+        points[i][1] = _mesh->point(vh)[1];
+        points[i][2] = _mesh->point(vh)[2];
+    }
+
+    // On donne aux points B et C leur ID
+    for(int i = 0; i<3 ; i++){
+        if(i != A){
+            if(B == -1)
+                B = i;
+            else if(C == -1)
+                C = i;
+        }
+    }
+
+    // Calcul des vecteurs AB et AC
+    VectorT <float,3> AB = points[B] - points[A];
+    VectorT <float,3> AC = points[C] - points[A];
+    VectorT <float,6> Vec;
+    Vec[0] = AB[0]; Vec[1] = AB[1]; Vec[2] = AB[2];
+    Vec[3] = AC[0]; Vec[4] = AC[1]; Vec[5] = AC[2];
+
+    return Vec;
+}
+
+
+
+VectorT <float,3> MainWindow::LongueurArc(MyMesh *_mesh, int vertexID, int vertexID2){
+
+    VectorT <float,3> points[2];
+
+    // affectation des points trouvés
+    for (int i=0; i<2;i++)
+    {
+        VertexHandle vh;
+
+        if(i == 0)
+            vh = _mesh->vertex_handle(vertexID);
+        else if(i == 1)
+            vh = _mesh->vertex_handle(vertexID2);
+
+        points[i][0] = _mesh->point(vh)[0];
+        points[i][1] = _mesh->point(vh)[1];
+        points[i][2] = _mesh->point(vh)[2];
+    }
+
+    // Calcul des vecteurs AB et AC
+    VectorT <float,3> AB = points[1] - points[0];
+
+    return AB;
+}
+
+
+float MainWindow::angleEE(MyMesh* _mesh, int vertexID,  int faceID)
+{
+    VectorT <float,6> vec = VecteurDirecteursTriangle(_mesh, vertexID, faceID);
+    VectorT <float,3> AB;
+        AB[0] = vec[0];
+        AB[1] = vec[1];
+        AB[2] = vec[2];
+    VectorT <float,3> AC;
+        AC[0] = vec[3];
+        AC[1] = vec[4];
+        AC[2] = vec[5];
+
+    AireBarycentrique(_mesh, vertexID);
+    return acos(AB.normalized()|AC.normalized());
+}
+
+
+
+float MainWindow::fctH(MyMesh* _mesh, int vertexID){
+
+    float firstPart = 1/(4.0*AireBarycentrique(_mesh, vertexID));
+    float secondPart = 0;
+
+    VertexHandle v_it = _mesh->vertex_handle(vertexID);
+    std::vector<VertexHandle> Vertexs;
+    for(MyMesh::VertexVertexIter  vv_it = mesh.vv_iter(v_it); vv_it; ++vv_it) {
+        VertexHandle vh = *vv_it;
+        Vertexs.push_back(vh);
+    }
+
+   int i = 0;
+   for(i =0; i<Vertexs.size()-1;i++) {
+       FaceHandle fh;
+       FaceHandle fh1;
+
+       bool first = false;
+       for (MyMesh::VertexFaceIter curVert = _mesh->vf_iter(v_it); curVert.is_valid(); curVert ++)
+       {
+           for (MyMesh::VertexFaceIter curVert1 = _mesh->vf_iter(Vertexs.at(i)); curVert1.is_valid(); curVert1 ++)
+           {
+               if((*curVert).idx() == (*curVert1).idx()){
+
+                   if(!first){
+                       fh = curVert;
+                       fh1 = curVert;
+                       first = true;
+                   }
+                   else
+                       fh1 = curVert;
+               }
+           }
+       }
+
+       int vertexEnFace = Vertexs.at(i).idx();
+
+       OpenMesh::Vec3f VecteurDirecteur = _mesh->point (_mesh->vertex_handle ( vertexEnFace ) ) - _mesh->point ( _mesh->vertex_handle(vertexID));
+
+       OpenMesh::Vec3f normal0 ( _mesh->normal ( fh ) );
+       OpenMesh::Vec3f normal1 ( _mesh->normal ( fh1 ) );
+
+       if (((normal0 % normal1) | VecteurDirecteur) < 0)
+       {
+           FaceHandle tmp = fh;
+           fh = fh1;
+           fh1 = tmp;
+       }
+        secondPart += (angleFF(_mesh, fh.idx(), fh1.idx(), vertexID, vertexEnFace) * VecteurDirecteur.norm());
+   }
+    return firstPart * secondPart;
+}
+
+
+void MainWindow::H_Curv(MyMesh* _mesh)
+{
+    for (MyMesh::VertexIter curVert = _mesh->vertices_begin(); curVert != _mesh->vertices_end(); curVert++)
+    {
+        VertexHandle vh = *curVert;
+        float value = fctH(_mesh, vh.idx());
+        _mesh->data(vh).value = value;
+    }
+}
+
+float MainWindow::AireBarycentrique(MyMesh* _mesh, int vertexID){
+    float aireTotal;
+     VertexHandle v_it = _mesh->vertex_handle(vertexID);
+
+     // parcours des faces autour de vertexID
+    for(MyMesh::VertexFaceIter  vf_it = mesh.vf_iter(v_it); vf_it; ++vf_it) {
+        FaceHandle fh = *vf_it;
+        aireTotal += faceArea(_mesh, fh.idx());
+    }
+    return 1/3.0*aireTotal;
+}
+
+float MainWindow::AngleAbs(MyMesh* _mesh, int vertexID){
+    float angleTotal;
+     VertexHandle v_it = _mesh->vertex_handle(vertexID);
+
+    // parcours des faces autour de vertexID
+    for(MyMesh::VertexFaceIter  vf_it = mesh.vf_iter(v_it); vf_it; ++vf_it) {
+        FaceHandle fh = *vf_it;
+        angleTotal += angleEE(_mesh, v_it.idx(), fh.idx());
+    }
+    return angleTotal;
+}
+
+float MainWindow::fctK(MyMesh* _mesh, int vertexID){
+    float firstPart = 1/AireBarycentrique(_mesh, vertexID);
+    float secondPart = 2*M_PI - AngleAbs(_mesh, vertexID);
+    return firstPart * secondPart;
+}
+
+void MainWindow::K_Curv(MyMesh* _mesh)
+{
+    for (MyMesh::VertexIter curVert = _mesh->vertices_begin(); curVert != _mesh->vertices_end(); curVert++)
+    {
+        VertexHandle vh = *curVert;
+        float value = fctK(_mesh, vh.idx());
+        _mesh->data(vh).value = value;
+    }
+}
+/* **** fin de la partie à compléter **** */
+
 
 
 /* **** début de la partie boutons et IHM **** */
+void MainWindow::on_pushButton_H_clicked()
+{
+    H_Curv(&mesh);
+    displayMesh(&mesh, true); // true permet de passer en mode "carte de temperatures", avec une gestion automatique de la couleur (voir exemple)
+}
 
+void MainWindow::on_pushButton_K_clicked()
+{
+    K_Curv(&mesh);
+    displayMesh(&mesh, true); // true permet de passer en mode "carte de temperatures", avec une gestion automatique de la couleur (voir exemple)
+}
 
-// exemple pour charger un fichier .obj
+/*
+    Cette fonction est à utiliser UNIQUEMENT avec le fichier testAngleArea.obj
+    Elle est appelée par le bouton "Test angles/aires"
+
+    Elle permet de vérifier les fonctions faceArea, angleFF et angleEE.
+    Elle doit afficher :
+
+    Aire de la face 0 : 2
+    Aire de la face 1 : 2
+    Angle entre les faces 0 et 1 : 1.5708
+    Angle entre les faces 1 et 0 : -1.5708
+    Angle au sommet 1 sur la face 0 : 0.785398
+*/
+
+void MainWindow::on_pushButton_angleArea_clicked()
+{
+    qDebug() << "Aire de la face 0 :" << faceArea(&mesh, 0);
+    qDebug() << "Aire de la face 1 :" << faceArea(&mesh, 1);
+
+    qDebug() << "Angle entre les faces 0 et 1 :" << angleFF(&mesh, 0, 1, 1, 2);
+    qDebug() << "Angle entre les faces 1 et 0 :" << angleFF(&mesh, 1, 0, 1, 2);
+
+    qDebug() << "Angle au sommet 1 sur la face 0 :" << angleEE(&mesh, 1, 0);
+    qDebug() << "Angle au sommet 3 sur la face 1 :" << angleEE(&mesh, 3, 1);
+}
+
 void MainWindow::on_pushButton_chargement_clicked()
 {
     // fenêtre de sélection des fichiers
@@ -22,83 +316,7 @@ void MainWindow::on_pushButton_chargement_clicked()
     // on affiche le maillage
     displayMesh(&mesh);
 }
-
-// exemple pour construire un mesh face par face
-void MainWindow::on_pushButton_generer_clicked()
-{
-    MyMesh mesh;
-
-    // on construit une liste de sommets
-    MyMesh::VertexHandle sommets[8];
-    sommets[0] = mesh.add_vertex(MyMesh::Point(-1, -1,  1));
-    sommets[1] = mesh.add_vertex(MyMesh::Point( 1, -1,  1));
-    sommets[2] = mesh.add_vertex(MyMesh::Point( 1,  1,  1));
-    sommets[3] = mesh.add_vertex(MyMesh::Point(-1,  1,  1));
-    sommets[4] = mesh.add_vertex(MyMesh::Point(-1, -1, -1));
-    sommets[5] = mesh.add_vertex(MyMesh::Point( 1, -1, -1));
-    sommets[6] = mesh.add_vertex(MyMesh::Point( 1,  1, -1));
-    sommets[7] = mesh.add_vertex(MyMesh::Point(-1,  1, -1));
-
-
-    // on construit des faces à partir des sommets
-
-    std::vector<MyMesh::VertexHandle> uneNouvelleFace;
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[0]);
-    uneNouvelleFace.push_back(sommets[1]);
-    uneNouvelleFace.push_back(sommets[2]);
-    uneNouvelleFace.push_back(sommets[3]);
-    mesh.add_face(uneNouvelleFace);
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[7]);
-    uneNouvelleFace.push_back(sommets[6]);
-    uneNouvelleFace.push_back(sommets[5]);
-    uneNouvelleFace.push_back(sommets[4]);
-    mesh.add_face(uneNouvelleFace);
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[1]);
-    uneNouvelleFace.push_back(sommets[0]);
-    uneNouvelleFace.push_back(sommets[4]);
-    uneNouvelleFace.push_back(sommets[5]);
-    mesh.add_face(uneNouvelleFace);
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[2]);
-    uneNouvelleFace.push_back(sommets[1]);
-    uneNouvelleFace.push_back(sommets[5]);
-    uneNouvelleFace.push_back(sommets[6]);
-    mesh.add_face(uneNouvelleFace);
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[3]);
-    uneNouvelleFace.push_back(sommets[2]);
-    uneNouvelleFace.push_back(sommets[6]);
-    uneNouvelleFace.push_back(sommets[7]);
-    mesh.add_face(uneNouvelleFace);
-
-    uneNouvelleFace.clear();
-    uneNouvelleFace.push_back(sommets[0]);
-    uneNouvelleFace.push_back(sommets[3]);
-    uneNouvelleFace.push_back(sommets[7]);
-    uneNouvelleFace.push_back(sommets[4]);
-    mesh.add_face(uneNouvelleFace);
-
-    mesh.update_normals();
-
-    // initialisation des couleurs et épaisseurs (sommets et arêtes) du mesh
-    resetAllColorsAndThickness(&mesh);
-
-    // on affiche le maillage
-    displayMesh(&mesh);
-
-}
-
 /* **** fin de la partie boutons et IHM **** */
-
-
 
 /* **** fonctions supplémentaires **** */
 // permet d'initialiser les couleurs et les épaisseurs des élements du maillage
@@ -307,6 +525,12 @@ void MainWindow::displayMesh(MyMesh* _mesh, bool isTemperatureMap, float mapRang
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
+    vertexSelection = -1;
+    edgeSelection = -1;
+    faceSelection = -1;
+
+    modevoisinage = false;
+
     ui->setupUi(this);
 }
 
@@ -314,5 +538,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-
--- mainwindow.h
+++ mainwindow.h
@@ -16,12 +16,12 @@ using namespace OpenMesh::Attributes;
 struct MyTraits : public OpenMesh::DefaultTraits
 {
     // use vertex normals and vertex colors
-    VertexAttributes( OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color | OpenMesh::Attributes::Status);
+    VertexAttributes( OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color );
     // store the previous halfedge
     HalfedgeAttributes( OpenMesh::Attributes::PrevHalfedge );
     // use face normals face colors
-    FaceAttributes( OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color | OpenMesh::Attributes::Status);
-    EdgeAttributes( OpenMesh::Attributes::Color | OpenMesh::Attributes::Status );
+    FaceAttributes( OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color );
+    EdgeAttributes( OpenMesh::Attributes::Color );
     // vertex thickness
     VertexTraits{float thickness; float value;};
     // edge thickness
@@ -38,17 +38,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    // les fonctions à compléter
+    VectorT <float,6> VecteurDirecteursTriangle(MyMesh *_mesh, int vertexID, int faceID);
+    VectorT <float,3> LongueurArc(MyMesh *_mesh, int vertexID, int vertexID2);
+
+
+    float faceArea(MyMesh* _mesh, int faceID);
+    float angleFF(MyMesh *_mesh, int faceID0, int faceID1, int vertID0, int vertID1);
+    float angleEE(MyMesh* _mesh, int vertexID, int faceID);
+    void H_Curv(MyMesh* _mesh);
+    void K_Curv(MyMesh* _mesh);
+    int PointEnFace(MyMesh *_mesh, int vertexID, int faceID, int faceID2);
+    float AireBarycentrique(MyMesh* _mesh, int vertexID);
+    float AngleAbs(MyMesh* _mesh, int vertexID);
+    float fctK(MyMesh* _mesh, int vertexID);
+    float fctH(MyMesh* _mesh, int vertexID);
+
     void displayMesh(MyMesh *_mesh, bool isTemperatureMap = false, float mapRange = -1);
     void resetAllColorsAndThickness(MyMesh* _mesh);
 
 private slots:
-    void on_pushButton_chargement_clicked();
 
-    void on_pushButton_generer_clicked();
+    void on_pushButton_chargement_clicked();
+    void on_pushButton_angleArea_clicked();
+    void on_pushButton_H_clicked();
+    void on_pushButton_K_clicked();
 
 private:
 
+    bool modevoisinage;
+
     MyMesh mesh;
+
+    int vertexSelection;
+    int edgeSelection;
+    int faceSelection;
 
     Ui::MainWindow *ui;
 };
